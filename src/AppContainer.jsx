60c6abbdc9741a0f40b991a9e2b5806c493ace41
Nicholas VanVoorthuysen <nicholas.vanvoorthuysen@gmail.com>
2017-02-01 12:59:19 -0500

    initial commit, working version migrated from sandbox

---
+++ src/AppContainer.jsx
@@ -0,0 +1,113 @@
+import React, { Component } from 'react'
+import greyWall from './image/P1010306-grey-wall.JPG'
+import portraitIconMD from './image/nv-mugshot-circle-icon100px-dither.png'
+import { Parallax, Background } from 'react-parallax'
+import './main.css'
+import './responsive.css'
+import 'flexboxgrid'
+import smoothScroll from 'smoothscroll-polyfill'
+
+export default class FlatParallax extends Component {
+  constructor(){
+    super()
+    this.state = ({ atBottom: false })
+    this.toggleScroll = this.toggleScroll.bind(this)
+    smoothScroll.polyfill()
+  }
+
+  toggleScroll() {
+    var vH = window.innerHeight
+    console.log('toggle function hit, direction:', this.state.atBottom, ' window height:', vH)
+    if(this.state.atBottom) {
+      window.scrollTo({top: 0, left: 0, behavior: 'smooth'})
+    // } else if (window.innerHeight < SET THIS MEDIA BREAKPOINT) {
+    } else {
+      window.scrollTo({top: vH, left: 0, behavior: 'smooth'})
+    }
+    this.setState({ atBottom: !this.state.atBottom })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="page-top"></div>
+        {/* <div className="container"> */}
+          <Parallax strength={400} blur={50}>
+            <div className="header-container">
+              <header className="header">
+                <div>
+                  <h1 className="me-title">Nicholas Vanvoorthuysen</h1>
+                </div>
+                {/* <div className="link-container"> */}
+                <h2 className="icon-wrapper">
+                  {/* <FontAwesome name="envelope" className="link" /> */}
+                  <a href="#" className="link">
+                    <i className="fa fa-envelope link"></i>
+                  </a>
+                  <a href="https://www.linkedin.com/in/nicholasvan" className="link">
+                    <i className="fa fa-linkedin-square link"></i>
+                  </a>
+                  <a href="https://github.com/NicholasJV" className="link">
+                    <i className="fa fa-github link"></i>
+                  </a>
+                  {/* <FontAwesome name="github" className="link" /> */}
+                  {/* <i className="fa fa-github"></i> */}
+                </h2>
+                {/* </div> */}
+                <h3 className="dev-title">software developer</h3>
+                <h1 className="dev-symbol">&#60;/></h1>
+              </header>
+            </div>
+            <Background blur={1000}>
+              <img src={greyWall}
+                className="background-image"
+                role="presentation"/>
+            </Background>
+            <div className="abt-section">
+              <h4 className="abt-title abt-text" onClick={this.toggleScroll}>
+                <span className="fa fa-angle-double-down"></span>
+                  <strong onClick={this.toggleScroll}> ABOUT </strong>
+                <span className="fa fa-angle-double-down"></span>
+              </h4>
+              {/* <div className="row-wrapper"> */}
+                <div className="row abt">
+                  <div className="abt-col col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <img src={portraitIconMD} className="portrait-icon" alt=""/>
+                    <strong className="abt-col-title"> Hi! </strong>
+                    <hr />
+                    <p>I'm Nick and I'm a software developer. </p>
+                    <p>With a background in engineering and video production,
+                    I am a versatile creator and determined problem-solver, excited and
+                    dedicated to building interesting user interfaces from sleek,
+                    minimal web applications to rich new media experiences. </p>
+                    <hr/>
+                  </div>
+                  <div className="abt-col col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <strong className="skills abt-col-title">CURRENT SKILLS:</strong>
+                    <hr />
+                    <ul className="skills-list">
+                      <li>Javascript/ES6</li>
+                      <li>HTML5</li>
+                      <li>React</li>
+                      <li>CSS3</li>
+                      <li>Angular</li>
+                      <li>Git & github</li>
+                      <li>Node.js</li>
+                      <li>Express.js</li>
+                      <li>MongoDB</li>
+                      <li>jQuery</li>
+                      <li>Bootstrap & Flexbox</li>
+                      <li>...more</li>
+                    </ul>
+                    <hr/>
+                  </div>
+                </div>
+              {/* </div> */}
+            </div>
+          </Parallax>
+        {/* </div> */}
+        <div className="page-bottom"></div>
+      </div>
+    );
+  }
+}
